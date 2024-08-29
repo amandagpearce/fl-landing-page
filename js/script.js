88a9f1f6ecd10ccf7629bb6f623baa945d6aee80
Amanda Pearce <amandagpearce@gmail.com>
2024-08-29 19:55:47 +0100

    adds hubspot form logic

--- js/script.js
+++ js/script.js
@@ -1,28 +1,52 @@
-// Brands caroussel
 document.addEventListener('DOMContentLoaded', function () {
-  new Splide('#splide', {
-    type: 'loop',
-    perPage: 8,
-    gap: '1rem',
-    // autoplay: true,
-    speed: 200000,
-    interval: 0,
-    pagination: false,
-    arrows: false,
-    breakpoints: {
-      768: {
-        perPage: 2,
+  const initBrandsSlider = () => {
+    new Splide('#splide', {
+      type: 'loop',
+      perPage: 8,
+      gap: '1rem',
+      // autoplay: true,
+      speed: 200000,
+      interval: 0,
+      pagination: false,
+      arrows: false,
+      breakpoints: {
+        768: {
+          perPage: 2,
+        },
+        480: {
+          perPage: 1,
+        },
       },
-      480: {
-        perPage: 1,
-      },
-    },
-  }).mount();
-});
+    }).mount();
+  };
 
-// Hubspot form
-// hbspt.forms.create({
-//   region: "na1",
-//   portalId: "20191864",
-//   formId: "e6f4dd61-1393-40c6-8a82-2dd87f364232"
-//   });
+  const initHubspotForm = () => {
+    const openLightbox = document.getElementById('open-lightbox');
+    const lightbox = document.getElementById('lightbox');
+    const closeLightbox = document.querySelector('.lightbox__close');
+
+    openLightbox.addEventListener('click', function () {
+      lightbox.style.display = 'flex';
+
+      hbspt.forms.create({
+        region: 'na1',
+        portalId: '20191864',
+        formId: 'e6f4dd61-1393-40c6-8a82-2dd87f364232',
+        target: '#hubspot-form-container',
+      });
+    });
+
+    closeLightbox.addEventListener('click', function () {
+      lightbox.style.display = 'none';
+    });
+
+    lightbox.addEventListener('click', function (e) {
+      if (e.target === lightbox) {
+        lightbox.style.display = 'none';
+      }
+    });
+  };
+
+  initBrandsSlider();
+  initHubspotForm();
+});
